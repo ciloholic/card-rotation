e18ba624b4eb3495e556c2ffe67944999d42c965
ciloholic <ciloholic0119@gmail.com>
2018-03-25 16:46:11 +0900

    ロジック修正

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,7 @@ export default {
   data: () => {
     return {
       cards: [
+        { id: 0, class: '', color: '', text: '周りが回る' },
         { id: 1, class: 'card01', color: '#e8aeb7', text: 'text-01' },
         { id: 2, class: 'card02', color: '#b8e1ff', text: 'text-02' },
         { id: 3, class: 'card03', color: '#a9fff7', text: 'text-03' },
@@ -18,25 +19,21 @@ export default {
         { id: 5, class: 'card05', color: '#82aba1', text: 'text-05' },
         { id: 6, class: 'card06', color: '#f9a03f', text: 'text-06' },
         { id: 7, class: 'card07', color: '#f7d488', text: 'text-07' },
-        { id: 8, class: 'card08', color: '#eaefb1', text: 'text-08' },
-        { id: 9, class: '', color: '', text: '周りが回る' }
+        { id: 8, class: 'card08', color: '#eaefb1', text: 'text-08' }
       ],
-      orders: {
-        card01: 'card02',
-        card02: 'card03',
-        card03: 'card05',
-        card05: 'card08',
-        card04: 'card01',
-        card06: 'card04',
-        card07: 'card06',
-        card08: 'card07'
-      }
+      MIN: 1,
+      MAX: 8
     }
   },
   created() {
     setInterval(() => {
       _.each(this.cards, card => {
-        card.class = this.orders[card.class]
+        let id = Number(card.class.slice(-2))
+        if (this.MAX <= id) {
+          card.class = 'card01'
+        } else if (this.MIN <= id && id < this.MAX) {
+          card.class = 'card' + ('00' + ++id).slice(-2)
+        }
       })
     }, 2000)
   }
@@ -59,8 +56,8 @@ ul {
   grid-gap: 10px;
   grid-template-areas:
     'card01 card02 card03'
-    'card04 ...... card05'
-    'card06 card07 card08';
+    'card08 ...... card04'
+    'card07 card06 card05';
 
   li {
     list-style: none;
